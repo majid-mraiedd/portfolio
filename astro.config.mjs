689df1d01688541b8370fba5403fb77653208046
majid <a.mraied@outlook.com>
2025-12-26 17:21:04 +0200

    Official 1.0 Release

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    // Your GitHub username: majid-mraiedd
+    // Your Repository name: portfolio
+    site: 'https://majid-mraiedd.github.io',
+    base: '/portfolio',
+});
